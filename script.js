dcc37eaf0b5653e4a0f64430253462ebfb6de566
avocado <avxcqdo@gmail.com>
2022-09-01 20:59:13 -0500

    rewrote smoothening algorithms

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // list of all values
 var map = [];
 
-// size of map (20x20)
+// size of map in cubes
 var mapsize = 20;
 
 // how many iterations the smoothen algorithm does
-var smoothen_iterations = 1
+var smoothen_iterations = 2;
 
 // elevation range (0-9)
 var elevationcount = 10;
@@ -14,7 +14,7 @@ var elevationcount = 10;
 var contrast = 10;
 
 // rgb weights
-var rgb = {r: 0, g: 3, b: 0}
+var rgbweight = { r: -2, g: -4, b: 0 };
 
 // canvas
 var canv = document.querySelector("#canv");
@@ -31,89 +31,112 @@ var cubesize = w / mapsize;
 var count = [];
 
 function init() {
-	generate();
+  generate();
+
+  document.querySelector("#mapsizeinput1").value = mapsize;
+  document.querySelector("#mapsizeinput2").value = mapsize;
+  document.querySelector("#smootheniterationsinput").value = smoothen_iterations;
 }
 
 function generate() {
-	// reset count and clear canvas
-	count = Array(64).fill(0);
-	map = [];
-	left = 0;
-	above = 0;
-	ctx.clearRect(0, 0, canv.width, canv.height);
+  // reset count and clear canvas
+  count = Array(64).fill(0);
+  map = [];
+  left = 0;
+  above = 0;
+  ctx.clearRect(0, 0, canv.width, canv.height);
 
-	// create the map
-	// random
-	for (let i = 0; i < mapsize; i++) {
-		// across
-		map[i] = [];
-		for (let e = 0; e < mapsize; e++) {
-			// down
-			map[i][e] = { elevation: Math.floor(Math.random() * elevationcount) };
-		}
-	}
+  // create the first map iteration, completely random
+  // adds 2 extra for the space on the ends
+  // 20x20 => 22x22
+  for (let i = 0; i < parseInt(mapsize) + 2; i++) {
+    // across
+    map[i] = [];
+    for (let e = 0; e < parseInt(mapsize) + 2; e++) {
+      // down
+      map[i][e] = { elevation: Math.floor(Math.random() * elevationcount) };
+    }
+  }
 
-	// iterate through again and smoothen
-	for(let p = 0; p< smoothen_iterations; p++){
-		for (let s = 1; s < mapsize-1; s++) {
-		// across
-		for (let c = 1; c < mapsize-1; c++) {
-			// down
-			// neighboring values
-			let u = map[s][c - 1].elevation // up
-			let ur = map[s -1][c +1].elevation
-		
-			let d = map[s][c + 1].elevation
-			let dl = map[s + 1][c-1].elevation
-			let dr = map[s +1][c+1].elevation
-			
-			let l = map[s - 1][c].elevation
-			let r = map[s + 1][c].elevation
+  // iterate through again and smoothen
+  for (let p = 0; p < smoothen_iterations; p++) {
+    // across
+    for (let s = 1; s < parseInt(mapsize) + 1; s++) {
+      // down
+      for (let c = 1; c < parseInt(mapsize) + 1; c++) {
+        console.log(s + ", " + c);
 
-			// average value
-			if(){
-				
-			}
-			let a = ( u + d+ l + r) / 4
+        // neighboring values
+        let ul = map[s - 1][c + 1].elevation; // up left
+        let u = map[s][c - 1].elevation; // up
+        let ur = map[s + 1][c + 1].elevation; // up right
+        let r = map[s + 1][c].elevation; // right
+        let dr = map[s + 1][c + 1].elevation; // down right
+        let d = map[s][c + 1].elevation; // down
+        let dl = map[s + 1][c - 1].elevation; // down left
+        let l = map[s - 1][c].elevation; // down left
 
-			// set value
-			map[s][c].elevation = a
-			console.log(a)
-		}
-	}
-	}
-	
+        // average value
+        let a = (ul + u + ur + r + dr + d + dl + l) / 8;
 
-	// display the map
-	for (let i = 0; i < map.length; i++) {
-		for (let e = 0; e < map[i].length; e++) {
-			// elevation color
-			ctx.fillStyle = `rgb(${255 - map[i][e].elevation * contrast},${255 - map[i][e].elevation * (contrast - 3)},${255 - map[i][e].elevation * contrast
-				})`;
-			// location
-			let x = i * cubesize;
-			let y = e * cubesize;
-			// draw cube
-			ctx.fillRect(x, y, cubesize, cubesize);
+        // set value
+        map[s][c].elevation = a;
 
-			count[Math.floor(map[i][e].elevation)] += 1;
-		}
-	}
-	document.querySelector("#mapsize").innerHTML = "Map Size: " + mapsize + "x" + mapsize;
-	document.querySelector("#cubecount").innerHTML = "Cube Count: " + mapsize * mapsize;
-		document.querySelector("#smootheniterations").innerHTML = "Smoothen Iterations: " + smoothen_iterations;
-	document.querySelector("#elevationcount").innerHTML = "Elevation Range: " + elevationcount;
-	let weight = 100 / (mapsize * mapsize);
-	document.querySelector("#count").innerHTML = "<strong>Elevation Distribution: (Floored)</strong><br>";
-	let max = Math.max(...count);
+        // if it's the last iteration, draw the cube
+        if (p == smoothen_iterations - 1) {
+          ctx.fillStyle = `rgb(
+            ${255 - map[s][c].elevation * (contrast + rgbweight.r)},
+            ${255 - map[s][c].elevation * (contrast + rgbweight.g)},
+            ${255 - map[s][c].elevation * (contrast + rgbweight.b)}
+      
+            )`;
+          // location
+          let x = (s - 1) * cubesize;
+          let y = (c - 1) * cubesize;
+          // draw cube
+          ctx.fillRect(x, y, cubesize, cubesize);
 
-	for (let p = 0; p < elevationcount; p++) {
-		if (p == count.indexOf(max)) {
-			document.querySelector("#count").innerHTML += '<strong>'+p + ": " + count[p] * weight + "% (" + count[p] + ")" + "</strong><br> ";
-		} else {
-			document.querySelector("#count").innerHTML += p + ": " + count[p] * weight + "% (" + count[p] + ")" + "<br> ";
-		}
-	}
+          count[Math.floor(map[s][c].elevation)] += 1;
+        }
+      }
+    }
+  }
+
+  if (smoothen_iterations == 0) {
+    for (let s = 1; s < parseInt(mapsize) + 1; s++) {
+      for (let c = 1; c < parseInt(mapsize) + 1; c++) {
+        ctx.fillStyle = `rgb(
+          ${255 - map[s][c].elevation * (contrast + rgbweight.r)},
+          ${255 - map[s][c].elevation * (contrast + rgbweight.g)},
+          ${255 - map[s][c].elevation * (contrast + rgbweight.b)}
+    
+          )`;
+        // location
+        let x = (s - 1) * cubesize;
+        let y = (c - 1) * cubesize;
+        // draw cube
+        ctx.fillRect(x, y, cubesize, cubesize);
+
+        count[Math.floor(map[s][c].elevation)] += 1;
+      }
+    }
+  }
+
+  document.querySelector("#mapsize").innerHTML = "Map Size: " + mapsize + "x" + mapsize;
+  document.querySelector("#cubecount").innerHTML = "Cube Count: " + mapsize * mapsize;
+  document.querySelector("#elevationcount").innerHTML = "Elevation Range: " + elevationcount;
+  let weight = 100 / (mapsize * mapsize);
+  document.querySelector("#count").innerHTML = "<strong>Elevation Distribution: (Floored)</strong><br>";
+  let max = Math.max(...count);
+
+  for (let p = 0; p < elevationcount; p++) {
+    if (p == count.indexOf(max)) {
+      document.querySelector("#count").innerHTML +=
+        "<strong>" + p + ": " + count[p] * weight + "% (" + count[p] + ")" + "</strong><br> ";
+    } else {
+      document.querySelector("#count").innerHTML += p + ": " + count[p] * weight + "% (" + count[p] + ")" + "<br> ";
+    }
+  }
 }
 
 init();
